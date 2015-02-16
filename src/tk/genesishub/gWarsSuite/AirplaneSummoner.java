25800612b01a8760306b52bec1703b4ca07983a1
aguy867 <aguy867@gmail.com>
2015-02-16 16:57:17 -0500

    Changes

--- src/tk/genesishub/gWarsSuite/AirplaneSummoner.java
+++ src/tk/genesishub/gWarsSuite/AirplaneSummoner.java
@@ -12,12 +12,14 @@ public class AirplaneSummoner {
 		boolean bool = false;
 		World world = Bukkit.getServer().getWorld("gWars");
 		Location location = p.getLocation();
+		int x = (int) location.getX();
+		int z = (int) location.getZ();
 		Location[] orange = {new Location(world, 425, 4, 200), new Location(world, 423, 4, 200), new Location(world, 427, 4, 200), new Location(world, 429, 4, 200)};
 		Location[] blue = {new Location(world, 562, 4, 132), new Location(world, 564, 4, 132),  new Location(world, 566, 4, 132), new Location(world, 568, 4, 132)};
 		for(int i = 421; i<=433; i++){
 			for(int iter = 164; iter<=204; iter++){
 				Bukkit.getLogger().info(i + " and " + iter);
-				if(location.getX() == i && location.getZ() == iter){
+				if(x == i && z == iter){
 					Bukkit.getLogger().info("HI");
 					int rand = (int) Math.ceil(Math.random()*4);
 						world.spawnEntity(orange[rand], EntityType.MINECART);
@@ -30,7 +32,7 @@ public class AirplaneSummoner {
 			for(int i = 560; i<=570; i++){
 				for(int iter = 128; iter<=167; iter++){
 					Bukkit.getLogger().info(i + " and " + iter);
-					if(location.getX() == i && location.getZ() == iter){
+					if(x == i && z == iter){
 						int rand = (int) Math.ceil(Math.random()*4);
 							world.spawnEntity(blue[rand], EntityType.MINECART);
 							Bukkit.getLogger().info(i + " and " + iter);
@@ -42,7 +44,7 @@ public class AirplaneSummoner {
 			for(int i = 565; i<=547; i++){
 				for(int iter = 427; iter<=430; iter++){
 					Bukkit.getLogger().info(i + " and " + iter);
-					if(location.getX() == i && location.getZ() == iter){
+					if(x == i && z == iter){
 						int rand = (int) Math.ceil(Math.random()*4);
 							world.spawnEntity(blue[rand], EntityType.MINECART);
 							bool = true;
