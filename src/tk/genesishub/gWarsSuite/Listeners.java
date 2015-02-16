5c4b921101f569cd88c71b81faadf9d38fd546d4
Seshpenguin <seshan10@me.com>
2015-02-16 15:13:26 -0500

    Added the Apache License
    Added the Apache License to the head Comment of the class file
    (listeners.class)

--- src/tk/genesishub/gWarsSuite/Listeners.java
+++ src/tk/genesishub/gWarsSuite/Listeners.java
@@ -25,8 +25,22 @@ import org.xml.sax.SAXException;
 
 import com.shampaggon.crackshot.events.WeaponDamageEntityEvent;
 /*
- * gWarsSuite2 Version 7.4
- * Bukkit Plugin by the GenesisHub Team
+   gWarsSuite2 Version 7.4
+   Bukkit Plugin by the GenesisHub Team
+   Copyright 2015 DolphinBox.net
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
  */
 
 public class Listeners extends JavaPlugin implements Listener{
