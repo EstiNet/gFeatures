5056214442b87ce4182f4974ff01e9ca5662c200
aguy867 <aguy867@gmail.com>
2015-02-16 19:09:14 -0500

    Boat Finish 2

--- src/tk/genesishub/gWarsSuite/BoatSummoner.java
+++ src/tk/genesishub/gWarsSuite/BoatSummoner.java
@@ -15,8 +15,8 @@ public class BoatSummoner {
 		int x = (int) location.getX();
 		int z = (int) location.getZ();
 		Location[] orange = {new Location(world, 414, 4, 218), new Location(world, 414, 4, 220), new Location(world, 414, 4, 226), new Location(world, 414, 4, 231)};
-		Location[] blue = {new Location(world, 562, 4, 132), new Location(world, 564, 4, 132),  new Location(world, 566, 4, 132), new Location(world, 568, 4, 132)};
-		Location[] island = {new Location(world, 550, 8, 415), new Location(world, 560, 8, 428)};
+		Location[] blue = {new Location(world, 577, 4, 112), new Location(world, 577, 4, 108),  new Location(world, 577, 4, 109), new Location(world, 577, 4, 112)};
+		Location[] island = {new Location(world, 545, 4, 441), new Location(world, 552, 4, 440), new Location(world, 559, 4, 441), new Location(world, 566, 4, 441)};
 		for(int i = 409; i<=420; i++){
 			for(int iter = 212; iter<=238; iter++){
 				if(x == i && z == iter){
@@ -39,10 +39,10 @@ public class BoatSummoner {
 						}
 					}
 				}
-			for(int i = 565; i<=547; i++){
-				for(int iter = 427; iter<=430; iter++){
+			for(int i = 537; i<=576; i++){
+				for(int iter = 433; iter<=443; iter++){
 					if(x == i && z == iter){
-						int rand = (int) Math.ceil(Math.random()*2);
+						int rand = (int) Math.ceil(Math.random()*4);
 							world.spawnEntity(island[rand], EntityType.BOAT);
 							bool = true;
 							p.sendMessage(ChatColor.BOLD+"A boat has been summoned near your area!");
