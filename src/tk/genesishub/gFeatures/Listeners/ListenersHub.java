7fdf117c516666fec2c0f2200a9a9873b35c8557
aguy867 <aguy867@gmail.com>
2015-02-18 19:15:26 -0500

    Changes

--- src/tk/genesishub/gFeatures/Listeners/ListenersHub.java
+++ src/tk/genesishub/gFeatures/Listeners/ListenersHub.java
@@ -7,10 +7,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
-
 import com.shampaggon.crackshot.events.WeaponDamageEntityEvent;
 
 public class ListenersHub {
+	tk.genesishub.gFeatures.gWarsSuite.Listeners gWars = new tk.genesishub.gFeatures.gWarsSuite.Listeners();
 	PlayerJoinEvent playerjoin;
 	PlayerQuitEvent playerquit;
 	PlayerMoveEvent playermove;
@@ -19,31 +19,103 @@ public class ListenersHub {
 	EntityDamageByEntityEvent damageentity;
 	WeaponDamageEntityEvent weaponevent;
 	PlayerInteractEvent playerinteract;
-	public ListenersHub(PlayerJoinEvent event){
+	protected ListenersHub(PlayerJoinEvent event){
 		playerjoin = event;
 	}
-	public ListenersHub(PlayerQuitEvent event){
+	protected ListenersHub(PlayerQuitEvent event){
 		playerquit = event;
 	}
-	public ListenersHub(PlayerMoveEvent event){
+	protected ListenersHub(PlayerMoveEvent event){
 		playermove = event;
 	}
-	public ListenersHub(PlayerRespawnEvent event){
+	protected ListenersHub(PlayerRespawnEvent event){
 		playerrespawn = event;
 	}
-	public ListenersHub(PlayerDeathEvent event){
+	protected ListenersHub(PlayerDeathEvent event){
 		playerdeath = event;
 	}
-	public ListenersHub(EntityDamageByEntityEvent event){
+	protected ListenersHub(EntityDamageByEntityEvent event){
 		damageentity = event;
 	}
-	public ListenersHub(WeaponDamageEntityEvent event){
+	protected ListenersHub(WeaponDamageEntityEvent event){
 		weaponevent = event;
 	}
-	public ListenersHub(PlayerInteractEvent event){
+	protected ListenersHub(PlayerInteractEvent event){
 		playerinteract = event;
 	}
-	public void PlayerJoinInitialize(){
+	protected void PlayerJoinInitialize(){
+		inTo();
+		try {
+			gWars.onPlayerJoin(playerjoin);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	protected void PlayerQuitInitialize(){
+		inTo();
+		try {
+			gWars.onPlayerJoin(playerjoin);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	protected void PlayerMoveInitialize(){
+		inTo();
+		try {
+			gWars.onPlayerJoin(playerjoin);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	protected void PlayerRespawnInitialize(){
+		inTo();
+		try {
+			gWars.onPlayerJoin(playerjoin);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	protected void PlayerDeathInitialize(){
+		inTo();
+		try {
+			gWars.onPlayerJoin(playerjoin);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	protected void EntityDamageEntityInitialize(){
+		inTo();
+		try {
+			gWars.onPlayerJoin(playerjoin);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	protected void WeaponDamageEntityInitialize(){
+		inTo();
+		try {
+			gWars.onPlayerJoin(playerjoin);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	protected void PlayerInteractInitialize(){
+		inTo();
+		try {
+			gWars.onPlayerJoin(playerjoin);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private void inTo(){
 		
 	}
 }
