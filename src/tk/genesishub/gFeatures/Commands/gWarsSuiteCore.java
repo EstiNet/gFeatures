382bd5d721b662b76d91b6b5d28717895fc0626c
aguy867 <aguy867@gmail.com>
2015-02-18 21:00:10 -0500

    Changes

--- src/tk/genesishub/gFeatures/Commands/gWarsSuiteCore.java
+++ src/tk/genesishub/gFeatures/Commands/gWarsSuiteCore.java
@@ -3,9 +3,12 @@ package tk.genesishub.gFeatures.Commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import tk.genesishub.gFeatures.gWarsSuite.CommandHub;
+
 public class gWarsSuiteCore {
-	public String CommandSwitch(final CommandSender sender, Command cmd, String label, String[] args){
-		
+	public String CommandSwitch(final CommandSender sender, Command cmd, String label, String[] args) throws NumberFormatException, Exception{
+		CommandHub ch = new CommandHub();
+		ch.CommandInitiate(sender, cmd, label, args);
 		return null;
 	}
 }
