c4ddf89a2f7708dedb1b896733a2d51fe3541977
aguy867 <aguy867@gmail.com>
2015-02-18 19:59:40 -0500

    Changes

--- src/tk/genesishub/gFeatures/Listeners/CommandCentral.java
+++ src/tk/genesishub/gFeatures/Listeners/CommandCentral.java
@@ -1,5 +1,10 @@
 package tk.genesishub.gFeatures.Listeners;
 
-public class CommandCentral {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 
+public class CommandCentral {
+	public void CommandInitiate(final CommandSender sender, Command cmd, String label, String[] args){
+		
+	}
 }
