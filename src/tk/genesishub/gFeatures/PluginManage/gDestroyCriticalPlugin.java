fd0cdf84169946c9c17be3499eee317712a42be3
aguy867 <aguy867@gmail.com>
2015-02-18 20:31:30 -0500

    Changes

---
+++ src/tk/genesishub/gFeatures/PluginManage/gDestroyCriticalPlugin.java
@@ -0,0 +1,11 @@
+package tk.genesishub.gFeatures.PluginManage;
+
+public class gDestroyCriticalPlugin {
+	PluginState state;
+	public gDestroyCriticalPlugin(PluginState state1){
+			this.state = state1;
+	}
+	public PluginState getState(){
+		return this.state;
+	}
+}
