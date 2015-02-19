cd6a123e82c03f47e1865e184bb7dadb71702219
aguy867 <aguy867@gmail.com>
2015-02-18 20:51:18 -0500

    Changes

---
+++ src/tk/genesishub/gFeatures/PluginManage/gFactionsPlugin.java
@@ -0,0 +1,18 @@
+package tk.genesishub.gFeatures.PluginManage;
+
+public class gFactionsPlugin {
+	PluginState state;
+	static double version;
+	public gFactionsPlugin(PluginState state1){
+			this.state = state1;
+	}
+	public gFactionsPlugin(double version1){
+		version = version1;
+	}
+	public double getVersion(){
+		return version;
+	}
+	public PluginState getState(){
+		return this.state;
+	}
+}
