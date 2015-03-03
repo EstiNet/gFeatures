1c03ef4467b3df21c0cf874ea4f1beb750c733fc
aguy867 <aguy867@gmail.com>
2015-03-02 20:38:02 -0500

    Changes

---
+++ src/tk/genesishub/gFeatures/GenesisEconomy/CommandHub.java
@@ -0,0 +1,10 @@
+package tk.genesishub.gFeatures.GenesisEconomy;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class CommandHub {
+	public void Intitiate(CommandSender sender, Command command, String commandLabel, String[] args) {
+		
+	}
+}
--- src/tk/genesishub/gFeatures/GenesisEconomy/Listeners.java
+++ src/tk/genesishub/gFeatures/GenesisEconomy/Listeners.java
@@ -1,6 +1,20 @@
 package tk.genesishub.gFeatures.GenesisEconomy;
 
+import java.util.logging.Logger;
+
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import net.milkbowl.vault.permission.Permission;
+
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import tk.genesishub.gFeatures.PluginManage.GenesisEconomyPlugin;
+import tk.genesishub.gFeatures.PluginManage.PluginState;
 
 /*
 gFeatures
@@ -22,10 +36,74 @@ https://github.com/Seshpenguin/gFeatures
 */
 
 public class Listeners {
+	private static final Logger log = Logger.getLogger("Minecraft");
+    public static Economy econ = null;
+    public static Permission perms = null;
+    public static Chat chat = null;
 	public void onEnable(){
 		Bukkit.getLogger().info("GenesisEconomy enabled. You're good to go!");
+		if (!setupEconomy() ) {
+            GenesisEconomyPlugin.setPluginState(PluginState.DISABLE);
+            return;
+        }
+        setupPermissions();
+        setupChat();
 	}
 	public void onDisable(){
 		Bukkit.getLogger().info("GenesisEconomy disabled. Good bye!");
 	}
+	private boolean setupEconomy() {
+        if (Bukkit.getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = Bukkit.getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = Bukkit.getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+        if(!(sender instanceof Player)) {
+            log.info("Only players are supported for this Example Plugin, but you should not do this!!!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if(command.getLabel().equals("test-economy")) {
+            // Lets give the player 1.05 currency (note that SOME economic plugins require rounding!)
+            sender.sendMessage(String.format("You have %s", econ.format(econ.getBalance(player.getName()))));
+            EconomyResponse r = econ.depositPlayer(player, 1.05);
+            if(r.transactionSuccess()) {
+                sender.sendMessage(String.format("You were given %s and now have %s", econ.format(r.amount), econ.format(r.balance)));
+            } else {
+                sender.sendMessage(String.format("An error occured: %s", r.errorMessage));
+            }
+            return true;
+        } else if(command.getLabel().equals("test-permission")) {
+            // Lets test if user has the node "example.plugin.awesome" to determine if they are awesome or just suck
+            if(perms.has(player, "example.plugin.awesome")) {
+                sender.sendMessage("You are awesome!");
+            } else {
+                sender.sendMessage("You suck!");
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
