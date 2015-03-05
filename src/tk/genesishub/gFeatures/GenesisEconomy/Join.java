730d9ddd673e347ac939ca667065c9eb7326584a
aguy867 <aguy867@gmail.com>
2015-03-04 20:36:47 -0500

    Changes

--- src/tk/genesishub/gFeatures/GenesisEconomy/Join.java
+++ src/tk/genesishub/gFeatures/GenesisEconomy/Join.java
@@ -4,6 +4,19 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Join {
 	public void Initialize(PlayerJoinEvent e){
-		
+		Connection c = new Connection();
+		CheckConfig cc = new CheckConfig();
+		c.Connect(c.toURL(cc.getPort(), cc.getAddress(), cc.getTablename()), cc.getUsername(), cc.getPassword(), "IF NOT EXISTS ("+
+		"SELECT * FROM information_schema.COLUMNS"+
+		"WHERE column_name=fieldName"+
+		"and table_name=tableName"+
+		"and table_schema=dbName"+
+		")"+
+	"THEN"+
+		"set @ddl=CONCAT('ALTER TABLE ',dbName,'.',tableName,"+
+			"' ADD COLUMN ',fieldName,' ',fieldDef);"+
+		"prepare stmt from @ddl;"+
+		"execute stmt;"
+	+"END IF");
 	}
 }
