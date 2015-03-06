1aeb172ae6820d1f643c696135a3f1e346145274
aguy867 <aguy867@gmail.com>
2015-03-05 21:19:39 -0500

    changes

--- src/tk/genesishub/gFeatures/GenesisEconomy/MoneyManager.java
+++ src/tk/genesishub/gFeatures/GenesisEconomy/MoneyManager.java
@@ -1,16 +1,32 @@
 package tk.genesishub.gFeatures.GenesisEconomy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.entity.Player;
 
 public class MoneyManager {
-	public double getMoney(Player p){
+	Connection c = new Connection();
+	CheckConfig cc = new CheckConfig();
+	String Address = cc.getAddress();
+	String Port = cc.getPort();
+	String Tablename = cc.getTablename();
+	String Username = cc.getUsername();
+	String Password = cc.getPassword();
+	String URL = c.toURL(Port, Address, Tablename);
+	public float getMoney(Player p){
+		List<String> rs = new ArrayList<>();
+		rs = c.ConnectReturn(URL, Username, Password, "SELECT Name, Money FROM People WHERE Name = '" + p.getUniqueId().toString() + "'");
+		return Float.parseFloat(rs.get(2));
+	}
+	public void giveMoney(Player p, float amount){
+		List<String> rs = new ArrayList<>();
+		rs = c.ConnectReturn(URL, Username, Password, "SELECT Name, Money FROM People WHERE Name = '" + p.getUniqueId().toString() + "'");
+		float num = Float.parseFloat(rs.get(2));
+		float money = num + amount;
+		rs = c.ConnectReturn(URL, Username, Password, "UPDATE People SET Money WHERE Name = '"+ p.getUniqueId().toString() +"'");
+	}
+	public void takeMoney(Player p, double amount){
 		
-		return (Double) null;
-	}
-	public double giveMoney(Player p, double amount){
-		return (Double) null;
-	}
-	public double takeMoney(Player p, double amount){
-		return (Double) null;
 	}
 }
