3823bbea1f471776fd2e13cbf78c1cf4afa5fa94
aguy867 <aguy867@gmail.com>
2015-03-17 14:58:55 -0400

    Changes

--- src/tk/genesishub/gFeatures/gScore/Listeners.java
+++ src/tk/genesishub/gFeatures/gScore/Listeners.java
@@ -2,18 +2,33 @@ package tk.genesishub.gFeatures.gScore;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.player.PlayerJoinEvent;
-
 import com.earth2me.essentials.api.UserDoesNotExistException;
 
 public class Listeners {
+	final Scored s = new Scored();
 	public void onEnable(){
 		Bukkit.getLogger().info("gScore is enabled! Stuff will happen!");
 	}
 	public void onDisable(){
 		Bukkit.getLogger().info("gScore is disabled! :(");
 	}
-	public void onPlayerJoin(PlayerJoinEvent event) throws IllegalArgumentException, IllegalStateException, UserDoesNotExistException{
-		Scored s = new Scored();
-		event.getPlayer().setScoreboard(s.Initialize(event.getPlayer()));
+	public void onPlayerJoin(final PlayerJoinEvent event) throws IllegalArgumentException, IllegalStateException, UserDoesNotExistException{
+		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Bukkit.getServer().getPluginManager().getPlugin("gFeatures"), new Runnable() {
+        	public void run(){
+        		try {
+					event.getPlayer().setScoreboard(s.Initialize(event.getPlayer()));
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UserDoesNotExistException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+           }
+        }, 100L);
+		Bukkit.getLogger().info("Scoreboard initiated.");
 	}
 }
--- src/tk/genesishub/gFeatures/gScore/Scored.java
+++ src/tk/genesishub/gFeatures/gScore/Scored.java
@@ -24,23 +24,26 @@ public class Scored {
 		Team team = board.registerNewTeam("GenesisHub");
 		Objective objective = board.registerNewObjective("test", "dummy");
 		objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-		objective.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "GenesisHub");
+		objective.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "_GenesisHub_");
 		Score score2 = objective.getScore(ChatColor.DARK_BLUE + "Online:"); //Get a fake offline player
 		score2.setScore(9);
 		Score score3 = objective.getScore(ChatColor.DARK_BLUE + "" + Integer.toString(Bukkit.getServer().getOnlinePlayers().length)); //Get a fake offline player
 		score3.setScore(8);
-		Score score4 = objective.getScore(""); //Get a fake offline player
-		score4.setScore(7);
 		Score score = objective.getScore(ChatColor.GREEN + "Clupic:"); //Get a fake offline player
-		score.setScore(6);
+		score.setScore(7);
 		Score score5 = objective.getScore(ChatColor.GREEN + "" + Float.toString(mm.getMoney(p))); //Get a fake offline player
-		score5.setScore(5);
-		Score score6 = objective.getScore(""); //Get a fake offline player
-		score6.setScore(4);
+		score5.setScore(6);
 		Score score1 = objective.getScore(ChatColor.AQUA + "Dollars:");
-		score1.setScore(3);
+		score1.setScore(5);
 		Score score7 = objective.getScore(ChatColor.AQUA + "" + Double.toString(Economy.getMoney(p.getName()))); //Get a fake offline player
-		score7.setScore(2);
+		score7.setScore(4);
+		Score score8 = objective.getScore(ChatColor.LIGHT_PURPLE + "Random Word:");
+		score8.setScore(3);
+		String[] str = {"Awesome!", "Epic!", "Amazing!", "Cool!", "I caz spll", "Dude!", "GenesisHub!", "Hi!", "Different!", "Oink." , "Beep!", "Welcome!", "Yo.", "LOL!"}; 
+		Score score9 = objective.getScore(ChatColor.LIGHT_PURPLE + str[(int) Math.floor(Math.random() * 14)]);
+		score9.setScore(2);
+		Score score10 = objective.getScore(ChatColor.GOLD + "" + ChatColor.BOLD + " Different.");
+		score10.setScore(1);
 		return board;
 	}
 }
