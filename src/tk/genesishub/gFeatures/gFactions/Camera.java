a6670cf09489d43635302c72e1da9eb8e33cff2d
aguy867 <aguy867@gmail.com>
2015-03-19 10:18:58 -0400

    Changes

--- src/tk/genesishub/gFeatures/gFactions/Camera.java
+++ src/tk/genesishub/gFeatures/gFactions/Camera.java
@@ -11,11 +11,13 @@ public class Camera {
 		p.hidePlayer(p);
 		World world = Bukkit.getServer().getWorld("Factions1.8.3");
 		Location loc = new Location(world, -95, 71, 51);
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " add camerastudio");
 		p.sendMessage(ChatColor.AQUA + "____" + ChatColor.GOLD + "Tutorial" + ChatColor.AQUA + "____");
 		p.sendMessage(ChatColor.DARK_AQUA + "Welcome to GenesisHub " + ChatColor.RED + " Factions" + ChatColor.DARK_AQUA + "!");
 		p.sendMessage(ChatColor.DARK_AQUA + "Let's get started!");
 		p.sendMessage(ChatColor.DARK_AQUA + "Now, this is our spawn.");
 		p.sendMessage(ChatColor.DARK_AQUA + "We'll be hovering here for a while.");
+		p.sendMessage(ChatColor.DARK_AQUA + "Sit back and enjoy the ride!");
 		p.sendMessage(ChatColor.AQUA + "____" + ChatColor.GOLD + "Tutorial" + ChatColor.AQUA + "____");
 		p.showPlayer(p);
 	}
