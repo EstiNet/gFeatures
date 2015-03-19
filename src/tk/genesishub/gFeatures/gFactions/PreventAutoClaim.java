c2730db6af86b47fae4ffa0c0b99a66ccd338174
aguy867 <aguy867@gmail.com>
2015-03-19 10:00:06 -0400

    Changes

--- src/tk/genesishub/gFeatures/gFactions/PreventAutoClaim.java
+++ src/tk/genesishub/gFeatures/gFactions/PreventAutoClaim.java
@@ -28,10 +28,29 @@ public class PreventAutoClaim {
 		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " add factions.promote");
 		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " add factions.relation");
 		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " add factions.name");
-		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " add -factions.*");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " add factions.title");
 	}
 	public void Initialize(RegionLeaveEvent event){
 		Player p = event.getPlayer();
 		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove -factions.*");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.create");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.demote*");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.description*");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.disband");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.faction");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.home");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.invite");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.join");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.leader");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.money");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.list");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.leave");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.open");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.player");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.motd");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.promote");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.relation");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.name");
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + p.getName() + " remove factions.title");
 	}
 }
