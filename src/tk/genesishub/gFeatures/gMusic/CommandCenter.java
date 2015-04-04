136f7e05f33cb92219004080171f4462e955e278
aguy867 <aguy867@gmail.com>
2015-04-03 21:44:31 -0400

    Changes

--- src/tk/genesishub/gFeatures/gMusic/CommandCenter.java
+++ src/tk/genesishub/gFeatures/gMusic/CommandCenter.java
@@ -2,11 +2,14 @@ package tk.genesishub.gFeatures.gMusic;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Effect;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 public class CommandCenter {
+	@SuppressWarnings("deprecation")
 	public void CommandInitiate(final CommandSender sender, Command cmd, String label, String[] args) throws Exception{
 		if(!(sender instanceof Player)){
 			Bukkit.getLogger().info("Players only!");
@@ -19,6 +22,14 @@ public class CommandCenter {
 			}
 			else if (args[0].equalsIgnoreCase("help")){
 				sender.sendMessage("----Music Help----");
+			}
+			else if (args[0].equalsIgnoreCase("stop")){
+				((Player) sender).playEffect(((Player) sender).getLocation(), Effect.RECORD_PLAY,
+                        (short) 0);
+                sender.sendMessage(ChatColor.RED + "Stopping any music....");
+			}
+			else{
+				sender.sendMessage(ChatColor.AQUA + "/music or /music stop");
 			}
 		}
 	}
