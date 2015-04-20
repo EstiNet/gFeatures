7cf6420cb583cf3b01801b46962759db3d96dacc
aguy867 <aguy867@gmail.com>
2015-04-20 18:10:58 -0400

    Changes

--- src/tk/genesishub/gFeatures/gUtilities/YML.java
+++ src/tk/genesishub/gFeatures/gUtilities/YML.java
@@ -11,9 +11,7 @@ public class YML {
 	public void create() throws IOException{
 			YamlConfiguration yamlFile = YamlConfiguration.loadConfiguration(f);
 			yamlFile.createSection("Config");
-			yamlFile.createSection("Config.Presets");
-			yamlFile.createSection("Config.Plugins");
-			yamlFile.createSection("Config.Skript");
+			yamlFile.createSection("Config.gBroadcast");
 			//Plugins
 			yamlFile.createSection("Config.Plugins.gWarsSuite");
 			yamlFile.createSection("Config.Plugins.gHub");
@@ -27,43 +25,11 @@ public class YML {
             yamlFile.createSection("Config.Plugins.gMusic");
             yamlFile.createSection("Config.Plugins.gUtilities");
             yamlFile.createSection("Config.Plugins.CTF");
-			//Presets
-			yamlFile.createSection("Config.Presets.gWars");
-			yamlFile.createSection("Config.Presets.gDestroy");
-			yamlFile.createSection("Config.Presets.gHub");
-			yamlFile.createSection("Config.Presets.gFactions");
-			//Skript
-			yamlFile.createSection("Config.Skript.gEssentialsHub");
-			yamlFile.createSection("Config.Skript.gEssentialsMinigames");
-			yamlFile.createSection("Config.Skript.gMMO");
-			yamlFile.createSection("Config.Skript.gEssentialsFactions");
-			yamlFile.createSection("Config.Skript.gCrates");
-			yamlFile.createSection("Config.Skript.gEssentialsGlobal");
-			//Presets value
+			//Plugins value
 			yamlFile.set("Config.Presets.gWars", "false");
 			yamlFile.set("Config.Presets.gDestroy", "false");
 			yamlFile.set("Config.Presets.gHub", "false");
 			yamlFile.set("Config.Presets.gFactions", "false");
-			//Plugins value
-			yamlFile.set("Config.Plugins.gWarsSuite", "false");
-			yamlFile.set("Config.Plugins.gHub", "false");
-			yamlFile.set("Config.Plugins.gFactions", "false");
-			yamlFile.set("Config.Plugins.GenesisAccess", "true");
-			yamlFile.set("Config.Plugins.gDestroyCritical", "false");
-			yamlFile.set("Config.Plugins.GenesisEconomy", "false");
-			yamlFile.set("Config.Plugins.GenesisBackup", "false");
-			yamlFile.set("Config.Plugins.gScore", "false");
-            yamlFile.set("Config.Plugins.HideAndSeek", "false");
-            yamlFile.set("Config.Plugins.gMusic", "false");
-            yamlFile.set("Config.Plugins.gUtilities", "false");
-            yamlFile.set("Config.Plugins.CTF", "false");
-			//Skript value
-			yamlFile.set("Config.Skript.gEssentialsHub", "false");
-			yamlFile.set("Config.Skript.gEssentialsMinigames", "false");
-			yamlFile.set("Config.Skript.gMMO", "false");
-			yamlFile.set("Config.Skript.gEssentialsFactions", "false");
-			yamlFile.set("Config.Skript.gCrates", "false");
-			yamlFile.set("Config.Skript.gEssentialsGlobal", "true");
 			yamlFile.save(f);
 			Bukkit.getLogger().info("Successfully added plugin data folders!");
 	}
