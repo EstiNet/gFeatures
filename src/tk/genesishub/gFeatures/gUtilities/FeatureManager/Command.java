04b99aea18d094d7c983524ff4990ecc21e23d0b
aguy867 <aguy867@gmail.com>
2015-04-30 19:58:54 -0400

    Changes

--- src/tk/genesishub/gFeatures/gUtilities/FeatureManager/Command.java
+++ src/tk/genesishub/gFeatures/gUtilities/FeatureManager/Command.java
@@ -3,7 +3,5 @@ package tk.genesishub.gFeatures.gUtilities.FeatureManager;
 import org.bukkit.command.CommandSender;
 
 public class Command {
-	public void onCommand(final CommandSender sender, Command cmd, String label, String[] args) throws Exception{
-		
-	}
+	public void onCommand(final CommandSender sender, Command cmd, String label, String[] args) throws Exception{}
 }
---
+++ src/tk/genesishub/gFeatures/gUtilities/FeatureManager/FeatureState.java
@@ -0,0 +1,5 @@
+package tk.genesishub.gFeatures.gUtilities.FeatureManager;
+
+public enum FeatureState {
+	ENABLE, DISABLE
+}
