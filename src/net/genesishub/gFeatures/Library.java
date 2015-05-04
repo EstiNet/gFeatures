3f78a352aea86dd234f74df1c424d721de9118f8
aguy867 <aguy867@gmail.com>
2015-05-04 18:58:28 -0400

    Changes

--- src/net/genesishub/gFeatures/Library.java
+++ src/net/genesishub/gFeatures/Library.java
@@ -1,9 +1,28 @@
 package net.genesishub.gFeatures;
 
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import com.shampaggon.crackshot.events.WeaponDamageEntityEvent;
 
 public class Library {
 	public void onPlayerJoin(PlayerJoinEvent event){}
 	public void onPlayerLeave(PlayerQuitEvent event){}
+    public void onPlayerMove(PlayerMoveEvent event){}
+    public void onPlayerRespawn(PlayerRespawnEvent event){}
+    public void onPlayerDeath(PlayerDeathEvent event){}
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {}
+    public void onWeaponDamageEntity(WeaponDamageEntityEvent event) {}
+    public void onPlayerInteract(PlayerInteractEvent event){}
+    public void onPlayerBreakBlock(BlockBreakEvent event){}
+    public void onPlayerChat(AsyncPlayerChatEvent event){}
+    public void onPlayerCommand(PlayerCommandPreprocessEvent event){}
 }
--- src/net/genesishub/gFeatures/Listeners.java
+++ src/net/genesishub/gFeatures/Listeners.java
@@ -1,18 +1,44 @@
 package net.genesishub.gFeatures;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Listeners extends JavaPlugin{
+import tk.genesishub.gFeatures.Listeners.CommandCentral;
+import tk.genesishub.gFeatures.Listeners.ListenersHub;
+
+import com.shampaggon.crackshot.events.WeaponDamageEntityEvent;
+
+public class Listeners extends JavaPlugin implements Listener{
 	public static final double version = 2.0;
 	
-	
+	PluginManager pm = getServer().getPluginManager();
+	Enabler enable = new Enabler();
+	Disabler disable = new Disabler();
+	Library library = new Library();
 	
 	public void onEnable(){
+	    pm.registerEvents(this, this);
 		getLogger().info(ChatColor.AQUA + "_________________________________________________________________________");
 		getLogger().info(ChatColor.GOLD + "[gFeatures] gFeatures enabled!");
 		getLogger().info(ChatColor.GOLD + "[gFeatures] This gFeatures installation is running core: " + version);
 		getLogger().info(ChatColor.GOLD + "[gFeatures] Turning on Features...");
+		enable.onEnable();
 		getLogger().info(ChatColor.GOLD + "[gFeatures] Complete!");
 		getLogger().info(ChatColor.AQUA + "_________________________________________________________________________");
 	}
@@ -21,7 +47,57 @@ public class Listeners extends JavaPlugin{
 		getLogger().info(ChatColor.GOLD + "[gFeatures] gFeatures disabled!");
 		getLogger().info(ChatColor.GOLD + "[gFeatures] This gFeatures installation is running core: " + version);
 		getLogger().info(ChatColor.GOLD + "[gFeatures] Turning off Features...");
+		disable.onDisable();
 		getLogger().info(ChatColor.GOLD + "[gFeatures] Complete!");
 		getLogger().info(ChatColor.AQUA + "_________________________________________________________________________");
 	}
+	@EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+    	library.onPlayerJoin(event);
+    }
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event){
+    	library.onPlayerLeave(event);
+    }
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event){
+    	library.onPlayerMove(event);
+    }
+    @EventHandler(priority=EventPriority.HIGHEST)
+    public void onPlayerRespawn(PlayerRespawnEvent event){
+    	library.onPlayerRespawn(event);
+    }
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event){
+    	library.onPlayerDeath(event);
+    }
+    @EventHandler
+    public void onHit(EntityDamageByEntityEvent event) {
+    	library.onEntityDamageByEntity(event);
+    }
+    @EventHandler
+    public void onHit1(WeaponDamageEntityEvent event) {
+        library.onWeaponDamageEntity(event);
+    }
+    @EventHandler
+    public void PlayerInteract(PlayerInteractEvent event){
+        library.onPlayerInteract(event);
+    }
+    @EventHandler
+    public void PlayerBreakBlock(BlockBreakEvent event){
+    	library.onPlayerBreakBlock(event);
+    }
+    @EventHandler
+    public void PlayerChatEvent(AsyncPlayerChatEvent event){
+    	library.onPlayerChat(event);
+    }
+    @EventHandler
+    public void PlayerCommandEvent(PlayerCommandPreprocessEvent event){
+    	library.onPlayerCommand(event);
+    }
+    @Override
+    public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args) {
+    	
+        return true;
+    }
 }
