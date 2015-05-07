314f3fd8486465ceca00217380be260c62b42c39
aguy867 <aguy867@gmail.com>
2015-05-06 20:10:35 -0400

    Changes

--- src/net/genesishub/gFeatures/Basic.java
+++ src/net/genesishub/gFeatures/Basic.java
@@ -1,10 +1,27 @@
 package net.genesishub.gFeatures;
 
-import net.genesishub.gFeatures.Configuration.MainConfig;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Basic {
-	public static MainConfig config = new MainConfig();
-	public static MainConfig getConfig(){
-		return config;
+	private static List<gFeature> features = new ArrayList<>();
+	private static List<Extension> extensions = new ArrayList<>();
+	public static void addFeature(gFeature feature){
+		features.add(feature);
+	}
+	public static void addExtension(Extension extend){
+		extensions.add(extend);
+	}
+	public static void removeFeature(gFeature feature){
+		features.remove(feature);
+	}
+	public static void removeExtension(Extension extend){
+		features.remove(extend);
+	}
+	public static List<gFeature> getFeatures(){
+		return features;
+	}
+	public static List<Extension> getExtensions(){
+		return extensions;
 	}
 }
