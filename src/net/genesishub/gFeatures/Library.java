6530c7180ffb3a69a23c6e878829a180b18e5ff5
aguy867 <aguy867@gmail.com>
2015-05-07 21:12:38 -0400

    Changes

--- src/net/genesishub/gFeatures/Library.java
+++ src/net/genesishub/gFeatures/Library.java
@@ -2,6 +2,7 @@ package net.genesishub.gFeatures;
 
 import java.util.List;
 
+import org.bukkit.event.Event;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -17,25 +18,48 @@ import com.shampaggon.crackshot.events.WeaponDamageEntityEvent;
 
 public class Library {
 	public void onPlayerJoin(PlayerJoinEvent event){
-		List<gFeature> features = Basic.getFeatures();
+		check("onPlayerJoin", event);
+	}
+	public void onPlayerLeave(PlayerQuitEvent event){
+		check("onPlayerLeave", event);
+	}
+    public void onPlayerMove(PlayerMoveEvent event){
+    	check("onPlayerMove", event);
+    }
+    public void onPlayerRespawn(PlayerRespawnEvent event){
+    	check("onPlayerRespawn", event);
+    }
+    public void onPlayerDeath(PlayerDeathEvent event){
+    	check("onPlayerDeath", event);
+    }
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+    	check("onEntityDamageByEntity", event);
+    }
+    public void onWeaponDamageEntity(WeaponDamageEntityEvent event) {
+    	check("onWeaponDamageEntity", event);
+    }
+    public void onPlayerInteract(PlayerInteractEvent event){
+    	check("onPlayerInteract", event);
+    }
+    public void onPlayerBreakBlock(BlockBreakEvent event){
+    	check("onPlayerBreakBlock", event);
+    }
+    public void onPlayerChat(AsyncPlayerChatEvent event){
+    	check("onPlayerChat", event);
+    }
+    public void onPlayerCommand(PlayerCommandPreprocessEvent event){
+    	check("onPlayerCommand", event);
+    }
+    public void check(String methodname, Event event){
+    	List<gFeature> features = Basic.getFeatures();
 		for(gFeature feature : features){
 			try {
-				if(!(feature.getClass().getDeclaredMethod("onPlayerJoin").equals(null))){
+				if(!(feature.getClass().getDeclaredMethod(methodname).equals(null))){
 					feature.eventTrigger(event);
 				}
 			} catch (NoSuchMethodException e) {
 			} catch (SecurityException e) {
 			}
 		}
-	}
-	public void onPlayerLeave(PlayerQuitEvent event){}
-    public void onPlayerMove(PlayerMoveEvent event){}
-    public void onPlayerRespawn(PlayerRespawnEvent event){}
-    public void onPlayerDeath(PlayerDeathEvent event){}
-    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {}
-    public void onWeaponDamageEntity(WeaponDamageEntityEvent event) {}
-    public void onPlayerInteract(PlayerInteractEvent event){}
-    public void onPlayerBreakBlock(BlockBreakEvent event){}
-    public void onPlayerChat(AsyncPlayerChatEvent event){}
-    public void onPlayerCommand(PlayerCommandPreprocessEvent event){}
+    }
 }
