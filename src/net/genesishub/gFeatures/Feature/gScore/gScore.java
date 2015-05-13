11d85e576ddd4c38af2f6e6933534a70db7370ab
aguy867 <aguy867@gmail.com>
2015-05-13 19:51:34 -0400

    gScore

--- src/net/genesishub/gFeatures/Feature/gScore/gScore.java
+++ src/net/genesishub/gFeatures/Feature/gScore/gScore.java
@@ -1,5 +1,62 @@
 package net.genesishub.gFeatures.Feature.gScore;
 
-public class gScore {
+import net.genesishub.gFeatures.Retrieval;
+import net.genesishub.gFeatures.gFeature;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import com.earth2me.essentials.api.UserDoesNotExistException;
+
+public class gScore extends gFeature{
+	
+	Listeners listeners = new Listeners();
+	
+	public gScore(String featurename, double d) {
+		super(featurename, d);
+	}
+	@Override
+	public void enable(){
+		listeners.onEnable();
+	}
+	@Override
+	public void disable(){
+		listeners.onDisable();
+	}
+	@Override
+	public void eventTrigger(Event event) {
+		if(event.getEventName().equalsIgnoreCase("playerjoinevent")){
+			try {
+				listeners.onPlayerJoin((PlayerJoinEvent)event);
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (UserDoesNotExistException e) {
+				e.printStackTrace();
+			}
+		}
+		else if(event.getEventName().equalsIgnoreCase("playerquitevent")){
+			try {
+				listeners.onPlayerLeave((PlayerQuitEvent)event);
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (UserDoesNotExistException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	@Retrieval
+	public void onPlayerJoin(){}
+	@Retrieval
+	public void onPlayerLeave(){}
+	@Override
+	public void commandTrigger(CommandSender sender, Command cmd, String label, String[] args) { 
+		//When stuff actually happens
+	}
 }
