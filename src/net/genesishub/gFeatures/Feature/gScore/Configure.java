13445c43e51e1620c8512bdf6d6870d321400161
aguy867 <aguy867@gmail.com>
2015-05-13 19:52:39 -0400

    gScore Configure

--- src/net/genesishub/gFeatures/Feature/gScore/Configure.java
+++ src/net/genesishub/gFeatures/Feature/gScore/Configure.java
@@ -1,5 +1,12 @@
 package net.genesishub.gFeatures.Feature.gScore;
 
-public class Configure {
+import net.genesishub.gFeatures.Basic;
+import net.genesishub.gFeatures.Configs;
 
+public class Configure {
+	@Configs
+	public static void onSetup(){
+		gScore base = new gScore("gScore", 1.0);
+		Basic.addFeature(base);
+	}
 }
