2721a4d81dbd5cdf4ba46e9201fb198376fa84b8
RYANNOOB28 <ryan.xr.tian@gmail.com>
2015-05-21 16:21:04 -0400

    Fixed critical spelling mistake by devin

--- src/net/genesishub/gFeatures/Feature/gWarsSuite/Enable.java
+++ src/net/genesishub/gFeatures/Feature/gWarsSuite/Enable.java
@@ -6,7 +6,7 @@ import net.genesishub.gFeatures.Basic;
 
 public class Enable {
 	public static void onEnable(){
-		Bukkit.getLogger().info("[gFeatures-gWarsSuite]gWarsSuite enabled! Running core version:s " + Basic.getFeature("gWarsSuite").getVersion() + "!");
+		Bukkit.getLogger().info("[gFeatures-gWarsSuite]gWarsSuite enabled! Running core version: " + Basic.getFeature("gWarsSuite").getVersion() + "!");
 		Basic.addPlayerSection("gWars", "");
 		Basic.addPlayerSection("gWars.Kills", "0");
 		Basic.addPlayerSection("gWars.Deaths", "0");
