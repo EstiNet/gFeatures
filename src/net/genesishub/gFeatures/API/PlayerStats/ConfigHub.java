1864d74de3a3776818c0fb5be596f3524f426e6b
aguy867 <aguy867@gmail.com>
2015-05-20 20:07:49 -0400

    Changes

--- src/net/genesishub/gFeatures/API/PlayerStats/ConfigHub.java
+++ src/net/genesishub/gFeatures/API/PlayerStats/ConfigHub.java
@@ -1,10 +1,35 @@
 package net.genesishub.gFeatures.API.PlayerStats;
 
+import java.io.File;
+import java.io.IOException;
+
+import net.genesishub.gFeatures.Configuration.Config;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
 public class ConfigHub {
+	static Config config = new Config();
+	static File f = new File("plugins/gFeatures/Players/players.yml");
+	
 	public void setupConfig(){
-		
+		Config config = new Config();
+		config.createDirectory("plugins/gFeatures/Players", "Setup the players directory for use!");
+		config.createFile("plugins/gFeatures/Players/players.yml", "Setup the players file for use!");
+		YamlConfiguration yamlFile = YamlConfiguration.loadConfiguration(f);
+		if(!(yamlFile.contains("Players"))){
+		yamlFile.createSection("Players");
+		}
+		try {
+			yamlFile.save(f);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	public void loadConfig(){
+		
+	}
+	public void addPlayerSection(Player p){
 		
 	}
 }
--- src/net/genesishub/gFeatures/Basic.java
+++ src/net/genesishub/gFeatures/Basic.java
@@ -1,22 +1,30 @@
 package net.genesishub.gFeatures;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Basic {
 	public static List<gFeature> features = new ArrayList<>();
 	private static List<Extension> extensions = new ArrayList<>();
+	private static HashMap<String, String> playersections;
 	public static void addFeature(gFeature feature){
 		features.add(feature);
 	}
 	public static void addExtension(Extension extend){
 		extensions.add(extend);
 	}
+	public static void addPlayerSection(String sectionname, String defaultvalue){
+		playersections.put(sectionname, defaultvalue);
+	}
 	public static void removeFeature(gFeature feature){
 		features.remove(feature);
 	}
 	public static void removeExtension(Extension extend){
-		features.remove(extend);
+		extensions.remove(extend);
+	}
+	public static void removePlayerSection(String sectionname, String defaultvalue){
+		playersections.remove(sectionname);
 	}
 	public static gFeature getFeature(String name){
 		for(gFeature feature : features){
@@ -39,5 +47,8 @@ public class Basic {
 	}
 	public static List<Extension> getExtensions(){
 		return extensions;
+	}
+	public static HashMap<String, String> getPlayerSections(){
+		return playersections;
 	}
 }
