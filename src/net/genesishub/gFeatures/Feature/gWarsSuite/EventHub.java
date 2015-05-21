0a85cced310559a598a919919baf2da442550cbd
aguy867 <aguy867@gmail.com>
2015-05-20 21:13:48 -0400

    Changes

--- src/net/genesishub/gFeatures/Feature/gWarsSuite/EventHub.java
+++ src/net/genesishub/gFeatures/Feature/gWarsSuite/EventHub.java
@@ -1,6 +1,10 @@
 package net.genesishub.gFeatures.Feature.gWarsSuite;
 
+import net.genesishub.gFeatures.Basic;
+import net.genesishub.gFeatures.API.PlayerStats.gPlayer;
+
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -9,6 +13,7 @@ public class EventHub {
 		Bukkit.getLogger().info("Player Joined!");
 	}
 	public void onPlayerQuit(PlayerQuitEvent event){
-		
+		Player p = event.getPlayer();
+		//MAKE SURE YOU RESET MODE
 	}
 }
