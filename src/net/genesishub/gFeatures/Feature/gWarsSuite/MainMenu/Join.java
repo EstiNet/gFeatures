b86337f4a4f96b72e813496e42db7b33fcb4fe2e
_InDev_ <aguy867@gmail.com>
2015-05-28 16:28:43 -0400

    Changes

--- src/net/genesishub/gFeatures/Feature/gWarsSuite/MainMenu/Join.java
+++ src/net/genesishub/gFeatures/Feature/gWarsSuite/MainMenu/Join.java
@@ -3,18 +3,31 @@ package net.genesishub.gFeatures.Feature.gWarsSuite.MainMenu;
 import net.genesishub.gFeatures.Feature.gWarsSuite.Constants;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.SkullType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
-public class Join {
+public strictfp class Join {
+	@SuppressWarnings("deprecation")
 	public void start(PlayerJoinEvent event){
 		//TODO PLAYER TELEPORT THINGY
 		Player p = event.getPlayer();
 		p.teleport(Constants.spawnonjoin);
-		for (Player players : Bukkit.getOnlinePlayers())
-        {
+		for (Player players : Bukkit.getOnlinePlayers()){
             players.hidePlayer(p);
         }
-		p.set
+		ItemStack item1 = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.CREEPER.ordinal());
+		SkullMeta item1meta = (SkullMeta) item1.getItemMeta();
+		
+		p.getInventory().setItem(0, item1);;
+	}
+	@SuppressWarnings("deprecation")
+	public void end(Player p){
+		for (Player players : Bukkit.getOnlinePlayers()){
+            players.showPlayer(p);
+        }
 	}
 }
