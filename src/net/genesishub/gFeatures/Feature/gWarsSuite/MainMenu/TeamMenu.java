f9caf5baeaf740cb6c5e232d74e08ae812e4cb20
_InDev_ <aguy867@gmail.com>
2015-05-30 11:46:31 -0400

    Changes

--- src/net/genesishub/gFeatures/Feature/gWarsSuite/MainMenu/TeamMenu.java
+++ src/net/genesishub/gFeatures/Feature/gWarsSuite/MainMenu/TeamMenu.java
@@ -4,6 +4,7 @@ import net.genesishub.gFeatures.API.Messaging.ActionAPI;
 import net.genesishub.gFeatures.Feature.gWarsSuite.Statistics;
 import net.genesishub.gFeatures.Feature.gWarsSuite.gWarsMode;
 import net.genesishub.gFeatures.Feature.gWarsSuite.Multiplayer.BlueTeam;
+import net.genesishub.gFeatures.Feature.gWarsSuite.Multiplayer.OrangeTeam;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -14,6 +15,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class TeamMenu {
+	GunMenu gm = new GunMenu();
 	Statistics stats = new Statistics();
 	public void initialize(Player p){
 		ItemStack item1 = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 3);
@@ -53,9 +55,26 @@ public class TeamMenu {
 		
 		if((event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.LEFT_CLICK_AIR))){
 			if(event.getPlayer().getItemInHand().equals(item1)){
-				
+				if(OrangeTeam.size() > BlueTeam.size()){
+					event.getPlayer().sendMessage(ChatColor.GRAY + "[Team] Can't join " + ChatColor.GOLD + "Kloyne" + ChatColor.GRAY + " because the teams will be unfair!");
+				}
+				else{
+					ActionAPI.sendActionBar(event.getPlayer(), ChatColor.AQUA + "Please select a kit.");
+					stats.setMode(event.getPlayer(), gWarsMode.GUNMENU);
+					gm.setup(event.getPlayer());
+				}
 			}
 			else if(event.getPlayer().getItemInHand().equals(item2)){
+				if(OrangeTeam.size() < BlueTeam.size()){
+					event.getPlayer().sendMessage(ChatColor.GRAY + "[Team] Can't join " + ChatColor.DARK_AQUA + "Innisfil" + ChatColor.GRAY + " because the teams will be unfair!");
+				}
+				else{
+					ActionAPI.sendActionBar(event.getPlayer(), ChatColor.AQUA + "Please select a kit.");
+					stats.setMode(event.getPlayer(), gWarsMode.GUNMENU);
+					gm.setup(event.getPlayer());
+				}
+			}
+			else if(event.getPlayer().getItemInHand().equals(item3)){
 				
 			}
 		}
