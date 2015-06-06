a2d2c4a46606d27e33e17f15f6d02cfbe388b36a
_InDev_ <aguy867@gmail.com>
2015-06-06 14:01:06 -0400

    Changes

--- src/net/genesishub/gFeatures/Feature/gWarsSuite/Configure.java
+++ src/net/genesishub/gFeatures/Feature/gWarsSuite/Configure.java
@@ -25,7 +25,7 @@ https://github.com/GenesisHub/gFeatures
 public class Configure {
 	@Configs
 	public static void onSetup(){
-		gWarsSuite base = new gWarsSuite("gWarsSuite", "3.1.11");
+		gWarsSuite base = new gWarsSuite("gWarsSuite", "3.2.1");
 		Basic.addFeature(base);
 	}
 }
--- src/net/genesishub/gFeatures/Feature/gWarsSuite/Multiplayer/Interact.java
+++ src/net/genesishub/gFeatures/Feature/gWarsSuite/Multiplayer/Interact.java
@@ -1,6 +1,7 @@
 package net.genesishub.gFeatures.Feature.gWarsSuite.Multiplayer;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.EntityType;
@@ -11,14 +12,41 @@ public class Interact {
 		World world = Bukkit.getServer().getWorld("gWars");
 		if(event.getClickedBlock().getLocation().equals(new Location(Bukkit.getServer().getWorld("gWars"), 657, 61, -56))){//Orange
 			int random = (int) Math.ceil(Math.random() * 4);
+			event.getPlayer().sendMessage(ChatColor.GRAY + "[Vehicle] Spawned a " + ChatColor.AQUA + "fighter jet" + ChatColor.GRAY + ".");
 			switch(random){
 			case 1:
 				world.spawnEntity(new Location(world, 651, 61, -54), EntityType.MINECART);
 				break;
+			case 2:
+				world.spawnEntity(new Location(world, 655, 61, -54), EntityType.MINECART);
+				break;
+			case 3:
+				world.spawnEntity(new Location(world, 659, 61, -54), EntityType.MINECART);
+				break;
+			case 4:
+				world.spawnEntity(new Location(world, 663, 61, -54), EntityType.MINECART);
+				break;
 			}
+			event.setCancelled(true);
 		}
-		else if(event.getClickedBlock().getLocation().equals(new Location(Bukkit.getServer().getWorld("gWars"), 657, 61, -56))){//Blue
-			
+		else if(event.getClickedBlock().getLocation().equals(new Location(Bukkit.getServer().getWorld("gWars"), 24, 61, 216))){//Orange
+			int random = (int) Math.ceil(Math.random() * 4);
+			event.getPlayer().sendMessage(ChatColor.GRAY + "[Vehicle] Spawned a " + ChatColor.AQUA + "fighter jet" + ChatColor.GRAY + ".");
+			switch(random){
+			case 1:
+				world.spawnEntity(new Location(world, 22, 61, 210), EntityType.MINECART);
+				break;
+			case 2:
+				world.spawnEntity(new Location(world, 22, 61, 214), EntityType.MINECART);
+				break;
+			case 3:
+				world.spawnEntity(new Location(world, 22, 61, 218), EntityType.MINECART);
+				break;
+			case 4:
+				world.spawnEntity(new Location(world, 22, 61, 222), EntityType.MINECART);
+				break;
+			}
+			event.setCancelled(true);
 		}
 	}
 }
