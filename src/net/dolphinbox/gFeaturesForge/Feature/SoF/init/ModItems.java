42191651010e843bdbe6c0b19969ed3c2932e44e
Seshpenguin <seshan10@me.com>
2015-06-21 20:56:57 -0400

    Changes

--- src/net/dolphinbox/gFeaturesForge/Feature/SoF/init/ModItems.java
+++ src/net/dolphinbox/gFeaturesForge/Feature/SoF/init/ModItems.java
@@ -3,11 +3,12 @@ package net.dolphinbox.gFeaturesForge.Feature.SoF.init;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.dolphinbox.gFeaturesForge.Feature.SoF.item.ItemQuestTracker;
 import net.dolphinbox.gFeaturesForge.Feature.SoF.item.ItemSoF;
+import net.minecraft.item.Item;
 
 public class ModItems {
 	public static final ItemSoF QuestTracker = new ItemQuestTracker();
 	
 	public static void init(){
-		//GameRegistry.registerItem(QuestTracker, "QuestTracker");
+		GameRegistry.registerItem(QuestTracker, "QuestTracker");
 	}
 }
