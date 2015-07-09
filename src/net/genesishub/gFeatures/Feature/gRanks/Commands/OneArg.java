61d3ec9aa63795b203d1ba2d4e8d7f5694a98c4e
_InDev_ <aguy867@gmail.com>
2015-07-09 16:20:18 -0400

    New UUIDFetcher API Integration

--- src/net/genesishub/gFeatures/Feature/gRanks/Commands/OneArg.java
+++ src/net/genesishub/gFeatures/Feature/gRanks/Commands/OneArg.java
@@ -1,10 +1,22 @@
 package net.genesishub.gFeatures.Feature.gRanks.Commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 public class OneArg {
 	public void onCommand(final CommandSender sender, Command cmd, String label, String[] args){
-		
+		if(args[0].equalsIgnoreCase("help")){
+			sender.sendMessage(ChatColor.GRAY + "------" + ChatColor.GOLD + "gRanks Help" + ChatColor.GRAY + "------");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks help - Shows the help page.");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks get [Player] - Gets the rank of a player.");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks set [Player] [Rank Name] - Sets the rank of a player.");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks create [Rank Name] [Rank Prefix]");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks setprefix [Rank Name] [Rank Prefix]");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks delete [Rank Name]");
+		}
+		else{
+			sender.sendMessage(ChatColor.GRAY + "[gRanks] Please do /gRanks help.");
+		}
 	}
 }
--- src/net/genesishub/gFeatures/Feature/gRanks/Commands/TwoArg.java
+++ src/net/genesishub/gFeatures/Feature/gRanks/Commands/TwoArg.java
@@ -1,10 +1,25 @@
 package net.genesishub.gFeatures.Feature.gRanks.Commands;
 
+import net.genesishub.gFeatures.Feature.gRanks.Retrieve;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 public class TwoArg {
+	Retrieve r = new Retrieve();
 	public void onCommand(final CommandSender sender, Command cmd, String label, String[] args){
-		
+		if(args[0].equalsIgnoreCase("get")){
+			sender.sendMessage(ChatColor.GRAY + "------" + ChatColor.GOLD + "gRanks Help" + ChatColor.GRAY + "------");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks help - Shows the help page.");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks get [Player] - Gets the rank of a player.");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks set [Player] [Rank Name] - Sets the rank of a player.");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks create [Rank Name] [Rank Prefix]");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks setprefix [Rank Name] [Rank Prefix]");
+			sender.sendMessage(ChatColor.GRAY + "/gRanks delete [Rank Name]");
+		}
+		else{
+			sender.sendMessage(ChatColor.GRAY + "[gRanks] Please do /gRanks help.");
+		}
 	}
 }
