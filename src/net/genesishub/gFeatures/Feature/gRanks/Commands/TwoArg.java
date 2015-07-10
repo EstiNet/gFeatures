0c7b31853117ee8d4dbbbf9fcb37767e2d8d2e30
_InDev_ <aguy867@gmail.com>
2015-07-10 17:05:08 -0400

    Changes

--- src/net/genesishub/gFeatures/Feature/gRanks/Commands/TwoArg.java
+++ src/net/genesishub/gFeatures/Feature/gRanks/Commands/TwoArg.java
@@ -28,24 +28,6 @@ public class TwoArg {
 			String rs = r.getRank(response.get(args[1]).toString());
 			sender.sendMessage(ChatColor.GRAY + "[gRanks] Player " + args[1] + " is " + rs);
 		}
-		else if(args[0].equalsIgnoreCase("set")){
-			if(!Basis.isRank(args[2])){
-				sender.sendMessage(ChatColor.RED + "[gRanks] Are you sure that " + args[2] + " is a rank?");
-			}
-			else{
-				UUIDFetcher uuid = new UUIDFetcher(Arrays.asList(args[1]));
-				Map<String, UUID> response = null;
-				try {
-					response = uuid.call();
-				} catch (Exception e) {
-					sender.sendMessage(ChatColor.RED + "[gRanks] Error with your input. Please try again!");
-					return;
-				}
-				Rank rank = Basis.getRank(args[2]);
-				r.setRank(rank, response.get(args[1]).toString());
-				sender.sendMessage(ChatColor.GRAY + "[gRanks] Set " + args[1] + "'s rank to " + args[2]);
-			}
-		}
 		else{
 			sender.sendMessage(ChatColor.GRAY + "[gRanks] Please do /gRanks help.");
 		}
