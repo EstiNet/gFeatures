ca0a7162ffe9f287bfac93cf83f4c3d83297e372
_InDev_ <aguy867@gmail.com>
2015-07-17 22:09:12 -0400

    Changes

--- src/net/genesishub/gFeatures/Feature/gRanks/Perms/Files.java
+++ src/net/genesishub/gFeatures/Feature/gRanks/Perms/Files.java
@@ -7,11 +7,13 @@ import net.genesishub.gFeatures.Feature.gRanks.Retrieve;
 
 public class Files {
 	Retrieve r = new Retrieve();
+	PermApp pa = new PermApp();
 	public void setupFiles(){
 		Config c = new Config();
 		c.createDirectory("plugins/gFeatures/gRanks/perms", "Created Permissions folder!");
 		for(Rank r : Basis.getRanks()){
 			c.createFile("plugins/gFeatures/gRanks/perms/" + r.getName() + ".txt", "Created permissions file for " + r.getName() + "!");
 		}
+		pa.setupPerms();
 	}
 }
