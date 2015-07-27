878d47be2f57203e1ff503a27dfab0d6335bed35
_InDev_ <aguy867@gmail.com>
2015-07-27 17:02:08 -0400

    Changes

--- src/net/genesishub/gFeatures/Feature/gRanks/Global/FileSync.java
+++ src/net/genesishub/gFeatures/Feature/gRanks/Global/FileSync.java
@@ -38,11 +38,11 @@ public class FileSync {
 			}
 			List<String> permdata = c.ConnectReturnPerm(URL, Username, Password, "SELECT * FROM Perms;");
 			for(int iter = 0; iter<permdata.size(); iter++){
+				try{
 				String perm = permdata.get(cache);
 				cache += 1;
 				String rank = permdata.get(cache);
 				cache += 1;
-				try{
 				BufferedWriter output = new BufferedWriter(new FileWriter(new File("plugins/gFeatures/gRanks/gperms/" + rank + ".txt"), true));
 				output.write(perm);
 				output.newLine();
