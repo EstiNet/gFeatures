65ab81959844d7c03102a77b4f198871648d9234
_InDev_ <aguy867@gmail.com>
2015-08-10 17:17:42 -0400

    Changes

--- src/net/genesishub/gFeatures/Feature/gRanks/LoopCheck.java
+++ src/net/genesishub/gFeatures/Feature/gRanks/LoopCheck.java
@@ -1,5 +1,6 @@
 package net.genesishub.gFeatures.Feature.gRanks;
 
+import net.genesishub.gFeatures.API.Logger.Debug;
 import net.genesishub.gFeatures.Feature.gRanks.Perms.Cleanup;
 
 import org.bukkit.Bukkit;
@@ -10,8 +11,14 @@ public class LoopCheck {
         	public void run(){
         		Thread th = new Thread(new Runnable(){
         			public void run(){
+        				try{
         				Basis b = new Basis();
         				b.initializeQuery();
+        				}
+        				catch(Exception e){
+        					Debug d = new Debug();
+        					d.print(e.getMessage());
+        				}
         			}
         		});
         		th.start();
@@ -19,8 +26,14 @@ public class LoopCheck {
         }, 1000L, 1000L);
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(Bukkit.getServer().getPluginManager().getPlugin("gFeatures"), new Runnable() {
         	public void run(){
+        		try{
         		Thread th = new Thread(new Cleanup());
         		th.start();
+        		}
+        		catch(Exception e){
+        			Debug d = new Debug();
+					d.print(e.getMessage());
+        		}
         	}
         }, 9000L, 9000L);
 	}
