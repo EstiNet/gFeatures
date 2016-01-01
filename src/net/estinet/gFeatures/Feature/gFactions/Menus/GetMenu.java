00fb335b49ac6068e46271af6493b0a17e1dfef0
_InDev_ <aguy867@gmail.com>
2016-01-01 11:37:48 -0500

    Changes

--- src/net/estinet/gFeatures/Feature/gFactions/Menus/GetMenu.java
+++ src/net/estinet/gFeatures/Feature/gFactions/Menus/GetMenu.java
@@ -2,14 +2,15 @@ package net.estinet.gFeatures.Feature.gFactions.Menus;
 
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
+import net.estinet.gFeatures.Basic;
 import net.estinet.gFeatures.Feature.gFactions.Blaze;
-import net.genesishub.gFeatures.Basic;
 
 public class GetMenu {
+	net.estinet.gFeatures.Feature.gFactions.Menus.Autorifle.Beginner autorifle = new net.estinet.gFeatures.Feature.gFactions.Menus.Autorifle.Beginner();
 	public void start(PlayerInteractEntityEvent event){
 		if(event.getRightClicked().getLocation().equals(Blaze.autorifle)){
 			if(!Basic.getgPlayer(event.getPlayer().getUniqueId().toString()).getValue("gFactions.Tier").equals("autorifle")){
-				
+				autorifle.makeInventory(event.getPlayer(), Basic.getgPlayer(event.getPlayer().getUniqueId().toString()));
 			}
 		}
 		else if(event.getRightClicked().getLocation().equals(Blaze.shotgun)){
