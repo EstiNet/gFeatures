1bc4e4eb68aa10b820bba5984e68cade3a80c2e4
_InDev_ <aguy867@gmail.com>
2016-01-01 10:53:49 -0500

    Changes

--- src/net/estinet/gFeatures/Feature/gFactions/EventHub.java
+++ src/net/estinet/gFeatures/Feature/gFactions/EventHub.java
@@ -3,6 +3,7 @@ package net.estinet.gFeatures.Feature.gFactions;
 import org.bukkit.entity.EnderCrystal;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.world.WorldLoadEvent;
 
@@ -22,6 +23,11 @@ public class EventHub {
 		Blaze.world.spawn(Blaze.sniper, EnderCrystal.class);
 		Blaze.world.spawn(Blaze.shotgun, EnderCrystal.class);
 		Blaze.world.spawn(Blaze.autorifle, EnderCrystal.class);
+		}
+	}
+	public void onEntityDamage(EntityDamageEvent event){
+		if(event.getEntityType().equals(EntityType.ENDER_CRYSTAL)){
+			event.setCancelled(true);
 		}
 	}
 }
