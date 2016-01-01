effc321fad14bbf9de3f939ad63a70a7e19696d8
_InDev_ <aguy867@gmail.com>
2016-01-01 15:26:57 -0500

    Changes

--- src/net/estinet/gFeatures/Feature/gFactions/Menus/GetMenu.java
+++ src/net/estinet/gFeatures/Feature/gFactions/Menus/GetMenu.java
@@ -11,6 +11,8 @@ import net.estinet.gFeatures.API.Inventory.InventoryAPI;
 
 public class GetMenu {
 	net.estinet.gFeatures.Feature.gFactions.Menus.Autorifle.Beginner autorifle = new net.estinet.gFeatures.Feature.gFactions.Menus.Autorifle.Beginner();
+	net.estinet.gFeatures.Feature.gFactions.Menus.Shotgun.Beginner shotgun = new net.estinet.gFeatures.Feature.gFactions.Menus.Shotgun.Beginner();
+	net.estinet.gFeatures.Feature.gFactions.Menus.Snipers.Beginner sniper = new net.estinet.gFeatures.Feature.gFactions.Menus.Snipers.Beginner();
 	public void start(PlayerInteractEntityEvent event){
 		Bukkit.getLogger().info(event.getPlayer().getUniqueId().toString());
 		Bukkit.getLogger().info(event.getRightClicked().getUniqueId().toString());
@@ -30,21 +32,23 @@ public class GetMenu {
 	}
 	public void start(EntityDamageByEntityEvent event){
 		Player pl = (Player) event.getDamager();
-		Bukkit.getLogger().info(pl.getUniqueId().toString());
-		Bukkit.getLogger().info(event.getEntity().getUniqueId().toString());
-		Bukkit.getLogger().info(Blaze.autorifles.toString());
 		if(event.getEntity().getUniqueId().toString().equals(Blaze.autorifles.toString())){
-			Bukkit.getLogger().info(Basic.getgPlayer(pl.getUniqueId().toString()).getValue("gFactionsTier"));
 			if(!(Basic.getgPlayer(pl.getUniqueId().toString()).getValue("gFactionsTier").equals("autorifle"))){
 				InventoryAPI iapi = autorifle.makeInventory(pl, Basic.getgPlayer(pl.getUniqueId().toString()));
 				iapi.open(pl);
 			}
 		}
 		else if(event.getEntity().getUniqueId().toString().equals(Blaze.shotguns.toString())){
-			
+			if(!(Basic.getgPlayer(pl.getUniqueId().toString()).getValue("gFactionsTier").equals("shotgun"))){
+				InventoryAPI iapi = shotgun.makeInventory(pl, Basic.getgPlayer(pl.getUniqueId().toString()));
+				iapi.open(pl);
+			}
 		}
 		else if(event.getEntity().getUniqueId().toString().equals(Blaze.snipers.toString())){
-			
+			if(!(Basic.getgPlayer(pl.getUniqueId().toString()).getValue("gFactionsTier").equals("sniper"))){
+				InventoryAPI iapi = autorifle.makeInventory(pl, Basic.getgPlayer(pl.getUniqueId().toString()));
+				iapi.open(pl);
+			}
 		}
 	}
 }
--- src/net/estinet/gFeatures/Feature/gFactions/Menus/Shotgun/Beginner.java
+++ src/net/estinet/gFeatures/Feature/gFactions/Menus/Shotgun/Beginner.java
@@ -1,6 +1,49 @@
 package net.estinet.gFeatures.Feature.gFactions.Menus.Shotgun;
 
-import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import net.estinet.gFeatures.Basic;
+import net.estinet.gFeatures.API.Inventory.InventoryAPI;
+import net.estinet.gFeatures.API.PlayerStats.gPlayer;
 
 public class Beginner {
+	public InventoryAPI makeInventory(final Player p, gPlayer player){
+		try{
+		InventoryAPI menu = new InventoryAPI("Shotgun Tier", 9, new InventoryAPI.OptionClickEventHandler() {
+	        @Override
+	        public void onOptionClick(final InventoryAPI.OptionClickEvent event) {
+	        	final BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+	            if(event.getName().equals(ChatColor.BOLD + "" + ChatColor.AQUA +  "Switch to Shotgun")){
+	            	event.getPlayer().closeInventory();
+	            	scheduler.scheduleSyncDelayedTask(Bukkit.getServer().getPluginManager().getPlugin("gFeatures"), new Runnable() {
+	                	public void run(){
+	                		p.sendMessage(ChatColor.BOLD + "[" + ChatColor.AQUA + "Tiers" + ChatColor.BOLD + "] " + ChatColor.DARK_AQUA + "You have joined the Shotgun tier.");
+	                		player.setValue("gFactionsTier", "shotgun");
+	                		Basic.setgPlayer(Basic.getgPlayer(event.getPlayer().getUniqueId().toString()), player);
+	                   }
+	                }, 9L);
+	            }
+	            event.setWillClose(true);
+	        }
+	    }, Bukkit.getServer().getPluginManager().getPlugin("gFeatures"))
+    .setOption(0, new ItemStack(Material.STAINED_GLASS_PANE, 1), ChatColor.BOLD + "Choose wisely!", "")
+    .setOption(1, new ItemStack(Material.STAINED_GLASS_PANE, 1), ChatColor.BOLD + "Choose wisely!", "")
+    .setOption(2, new ItemStack(Material.STAINED_GLASS_PANE, 1), ChatColor.BOLD + "Choose wisely!", "")
+    .setOption(3, new ItemStack(Material.STAINED_GLASS_PANE, 1), ChatColor.BOLD + "Choose wisely!", "")
+    .setOption(4, new ItemStack(Material.ENCHANTED_BOOK, 1), ChatColor.BOLD + "" + ChatColor.AQUA +  "Switch to Shotgun", ChatColor.RED + "" + ChatColor.BOLD + "Are you sure you want to switch to the shotgun tier?")
+    .setOption(5, new ItemStack(Material.STAINED_GLASS_PANE, 1), ChatColor.BOLD + "Choose wisely!", "")
+    .setOption(6, new ItemStack(Material.STAINED_GLASS_PANE, 1), ChatColor.BOLD + "Choose wisely!", "")
+    .setOption(7, new ItemStack(Material.STAINED_GLASS_PANE, 1), ChatColor.BOLD + "Choose wisely!", "")
+    .setOption(8, new ItemStack(Material.STAINED_GLASS_PANE, 1), ChatColor.BOLD + "Choose wisely!", "");
+	return menu;
+	}catch(Exception e){
+		e.printStackTrace();
+	}
+		return null;
+    }
 }
