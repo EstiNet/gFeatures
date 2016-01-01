a0524f097bcfcebb4da5f407d8bd23470edcd228
_InDev_ <aguy867@gmail.com>
2016-01-01 09:58:04 -0500

    Changes

---
+++ src/net/estinet/gFeatures/Feature/gFactions/Blaze.java
@@ -0,0 +1,12 @@
+package net.estinet.gFeatures.Feature.gFactions;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+public class Blaze {
+	public static World world = Bukkit.getWorld("Factions1.8.3");
+	public static Location sniper = new Location(Bukkit.getWorld("Factions1.8.3"), -261, 96, 127);
+	public static Location shotgun = new Location(Bukkit.getWorld("Factions1.8.3"), -257, 97, 129);
+	public static Location autorifle = new Location(Bukkit.getWorld("Factions1.8.3"), -222, 97, 153);
+}
--- src/net/estinet/gFeatures/Feature/gFactions/Enable.java
+++ src/net/estinet/gFeatures/Feature/gFactions/Enable.java
@@ -1,7 +1,13 @@
 package net.estinet.gFeatures.Feature.gFactions;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EnderCrystal;
+
 public class Enable {
 	public void plzenable(){
-		
+		Bukkit.getLogger().info("gFactions initialized! Yay!");
+		Blaze.world.spawn(Blaze.sniper, EnderCrystal.class);
+		Blaze.world.spawn(Blaze.shotgun, EnderCrystal.class);
+		Blaze.world.spawn(Blaze.autorifle, EnderCrystal.class);
 	}
 }
