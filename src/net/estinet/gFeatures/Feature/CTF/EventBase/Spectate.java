907b317ee0508631be6d7a18f7c5631f1670224f
_InDev_ <aguy867@gmail.com>
2016-01-21 18:49:01 -0500

    Changes

--- src/net/estinet/gFeatures/Feature/CTF/EventBase/Spectate.java
+++ src/net/estinet/gFeatures/Feature/CTF/EventBase/Spectate.java
@@ -1,9 +1,10 @@
 package net.estinet.gFeatures.Feature.CTF.EventBase;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 
 public class Spectate {
 	public void handler(Player p){
-		
+		p.setGameMode(GameMode.SPECTATOR);
 	}
 }
