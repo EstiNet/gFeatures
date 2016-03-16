8c7a2ddce7e67c99ece518a483bf3f405a5d3bca
_InDev_ <aguy867@gmail.com>
2016-03-16 10:03:54 -0400

    Revert "Changes"
    This reverts commit 49b26eef8d3321db37f71933a464aed204a49503.

--- src/net/estinet/gFeatures/Feature/CTF/Holo/CTFScore.java
+++ src/net/estinet/gFeatures/Feature/CTF/Holo/CTFScore.java
@@ -38,41 +38,21 @@ public class CTFScore {
 		objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 		objective.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "     _Esti" + ChatColor.DARK_AQUA + "" + ChatColor.BOLD +"Net_     ");
 		Score score2 = objective.getScore(ChatColor.DARK_BLUE + "Online:"); //Get a fake offline player
-		score2.setScore(12);
+		score2.setScore(8);
 		Score score3 = objective.getScore(ChatColor.BOLD + "" + Integer.toString(Bukkit.getServer().getOnlinePlayers().size())); //Get a fake offline player
-		score3.setScore(11);
+		score3.setScore(7);
 		Score score7 = objective.getScore(ChatColor.GREEN + "Time left:"); //Get a fake offline player
-		score7.setScore(10);
+		score7.setScore(6);
 		Score score10 = objective.getScore(ChatColor.BOLD + "" + Basic.minutes + "m " + Basic.seconds + "s"); //Get a fake offline player
-		score10.setScore(9);
+		score10.setScore(5);
 		Score score = objective.getScore(ChatColor.AQUA + "Flags Captured (Blue):"); //Get a fake offline player
-		score.setScore(8);
+		score.setScore(4);
 		Score score5 = objective.getScore(ChatColor.BOLD + "" + Basic.blueflags + " "); //Get a fake offline player
-		score5.setScore(7);
+		score5.setScore(3);
 		Score score8 = objective.getScore(ChatColor.GOLD + "Flags Captured (Orange):");
-		score8.setScore(6);
+		score8.setScore(2);
 		Score score9 = objective.getScore(ChatColor.BOLD + "" + Basic.orangeflags);
-		score9.setScore(5);
-		Score score12 = objective.getScore(ChatColor.AQUA + "Flag Taker (Blue):");
-		score12.setScore(4);
-		if(Basic.blueflagger.equals(null)){
-		Score score13 = objective.getScore(ChatColor.BOLD + "" + ChatColor.GOLD + "No one.");
-		score13.setScore(3);
-		}
-		else{
-			Score score13 = objective.getScore(ChatColor.BOLD + "" + ChatColor.GOLD + Basic.blueflagger.getName());
-			score13.setScore(3);
-		}
-		Score score14 = objective.getScore(ChatColor.GOLD + "Flag Taker (Orange):");
-		score14.setScore(2);
-		if(Basic.orangeflagger.equals(null)){
-			Score score15 = objective.getScore(ChatColor.BOLD + "" + ChatColor.AQUA + "No one.");
-			score15.setScore(1);
-		}
-		else{
-			Score score15 = objective.getScore(ChatColor.BOLD + "" + ChatColor.AQUA + Basic.orangeflagger.getName());
-			score15.setScore(1);
-		}
+		score9.setScore(1);
 		return board;
 	}
 }
