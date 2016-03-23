16f986b4da543c981e1833a68837dfe3081305e1
_InDev_ <aguy867@gmail.com>
2016-03-23 18:43:42 -0400

    Changes

--- src/net/estinet/gFeatures/ClioteSky/Network/NetworkThread.java
+++ src/net/estinet/gFeatures/ClioteSky/Network/NetworkThread.java
@@ -1,10 +1,41 @@
 package net.estinet.gFeatures.ClioteSky.Network;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import net.estinet.gFeatures.ClioteSky.ClioteSky;
+
 public class NetworkThread {
+	public static Socket clientSocket = null;
+	public static DataOutputStream outToServer = null;
+	public static BufferedReader inFromServer = null;
 	public static void start(){
-		
+		try{
+			String input;
+			clientSocket = new Socket(ClioteSky.getAddress(), Integer.parseInt(ClioteSky.getPort()));
+			outToServer = new DataOutputStream(clientSocket.getOutputStream());
+			inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			while(true){
+				try{
+				input = inFromServer.readLine();
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
-	public static void sendOutput(){
-		
+	public static void sendOutput(String message){
+		try {
+			outToServer.writeBytes(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
