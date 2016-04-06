e95f0e480715ef25b783f18fcd11a8f72e1c99d9
_InDev_ <aguy867@gmail.com>
2016-04-06 17:11:23 -0400

    Changes

--- src/net/estinet/gFeatures/Listeners.java
+++ src/net/estinet/gFeatures/Listeners.java
@@ -56,7 +56,7 @@ https://github.com/EstiNet/gFeatures
  */
 
 public class Listeners extends JavaPlugin implements Listener{
-	public static final String version = "3.3.0";
+	public static final String version = "3.3.1";
 	public static boolean debug = false;
 
 	PluginManager pm = getServer().getPluginManager();
