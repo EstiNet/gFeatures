3ef7040c7356a3d5d6d73a3aaaf652079faaad0d
_InDev_ <aguy867@gmail.com>
2016-05-07 19:49:22 -0400

    Changes

--- src/net/estinet/gFeatures/Feature/EstiChat/EventHub.java
+++ src/net/estinet/gFeatures/Feature/EstiChat/EventHub.java
@@ -27,12 +27,12 @@ https://github.com/EstiNet/gFeatures
 public class EventHub{
 	public void onPlayerChat(AsyncPlayerChatEvent event){
 		CliotePing cp = new CliotePing();
-		cp.sendMessage("chat <" + event.getPlayer().getDisplayName() + "> " + event.getMessage(), "Bungee");
+		cp.sendMessage("chat " + event.getPlayer().getName() + " <" + event.getPlayer().getDisplayName() + "> " + event.getMessage(), "Bungee");
 		EstiChat.lastSent = event.getMessage();
 	}
 	public void onPlayerDeath(PlayerDeathEvent event){
 		CliotePing cp = new CliotePing();
-		cp.sendMessage("chat " + event.getDeathMessage(), "Bungee");
+		cp.sendMessage("chat "+ event.getEntity().getName() + " " + event.getDeathMessage(), "Bungee");
 		EstiChat.lastSent = event.getDeathMessage();
 	}
 }
