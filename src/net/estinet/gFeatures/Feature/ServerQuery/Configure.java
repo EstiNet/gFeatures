80ede7a236aed8d3ea5409636be44ee48078cbf0
_InDev_ <aguy867@gmail.com>
2016-07-25 18:29:45 -0400

    ServerQuery 1.0.0

--- src/net/estinet/gFeatures/Feature/ServerQuery/Configure.java
+++ src/net/estinet/gFeatures/Feature/ServerQuery/Configure.java
@@ -2,10 +2,14 @@ package net.estinet.gFeatures.Feature.ServerQuery;
 
 import net.estinet.gFeatures.Basic;
 import net.estinet.gFeatures.Configs;
+import net.estinet.gFeatures.ClioteSky.ClioteSky;
 public class Configure{
 	@Configs
 	public static void onSetup(){
 		ServerQuery base = new ServerQuery("ServerQuery", "1.0.0");
 		Basic.addFeature(base);
+		
+		ServerQueryClioteHook sqch = new ServerQueryClioteHook(base);
+		ClioteSky.addClioteHook(sqch);
 	}
 }
--- src/net/estinet/gFeatures/Feature/ServerQuery/ServerQueryClioteHook.java
+++ src/net/estinet/gFeatures/Feature/ServerQuery/ServerQueryClioteHook.java
@@ -2,8 +2,14 @@ package net.estinet.gFeatures.Feature.ServerQuery;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import net.estinet.gFeatures.Basic;
+import net.estinet.gFeatures.FeatureState;
 import net.estinet.gFeatures.gFeature;
 import net.estinet.gFeatures.ClioteSky.API.ClioteHook;
+import net.estinet.gFeatures.Feature.gScore.Listeners;
 
 public class ServerQueryClioteHook extends ClioteHook{
 
@@ -14,6 +20,12 @@ public class ServerQueryClioteHook extends ClioteHook{
 	public void run(List<String> args, String categoryName, String clioteName){
 		if(args.get(0).equals("online")){
 			ServerQuery.setPlayerCount(Integer.parseInt(args.get(1)));
+			if(Basic.getFeature("gScore").getState().equals(FeatureState.ENABLE)){
+				Listeners l = new Listeners();
+				for(Player p : Bukkit.getOnlinePlayers()){
+					l.set(p);
+				}
+			}
 		}
 		else if(args.get(0).equals("playerget")){
 			Execute e = ServerQuery.hash.get(args.get(1));
