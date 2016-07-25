9f3f21c7046ab7e7c7392ade8c58a3df0efa8bea
_InDev_ <aguy867@gmail.com>
2016-07-25 14:17:56 -0400

    Changes

--- src/net/estinet/gFeatures/Feature/gScore/Scored.java
+++ src/net/estinet/gFeatures/Feature/gScore/Scored.java
@@ -38,20 +38,28 @@ public class Scored {
 		Scoreboard board = manager.getNewScoreboard();
 		Objective objective = board.registerNewObjective("test", "dummy");
 		objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-		objective.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "     _Esti" + ChatColor.DARK_AQUA + "" + ChatColor.BOLD +"Net_     ");
-		Score score2 = objective.getScore(ChatColor.DARK_BLUE + "Online:"); //Get a fake offline player
-		score2.setScore(7);
-		Score score3 = objective.getScore(ChatColor.DARK_BLUE + "" + Integer.toString(Bukkit.getServer().getOnlinePlayers().size())); //Get a fake offline player
-		score3.setScore(6);
-		Score score = objective.getScore(ChatColor.GREEN + "Clupic:"); //Get a fake offline player
-		score.setScore(5);
-		Score score5 = objective.getScore(ChatColor.GREEN + "" + Float.toString(mm.getMoney(p))); //Get a fake offline player
-		score5.setScore(4);
-		Score score8 = objective.getScore(ChatColor.LIGHT_PURPLE + "Random Word:");
-		score8.setScore(3);
+		objective.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "     Esti" + ChatColor.DARK_AQUA + "" + ChatColor.BOLD +"Net     ");
+		Score score1 = objective.getScore("");
+		score1.setScore(11);
+		Score score2 = objective.getScore(ChatColor.DARK_BLUE + "" + ChatColor.BOLD + "Online");
+		score2.setScore(10);
+		Score score3 = objective.getScore(ChatColor.GRAY + "" + Integer.toString(Bukkit.getServer().getOnlinePlayers().size()));
+		score3.setScore(9);
+		Score score12 = objective.getScore(" ");
+		score12.setScore(8);
+		Score score = objective.getScore(ChatColor.GREEN + "" + ChatColor.BOLD + "Clupic ⛀⛁⛃⛂");
+		score.setScore(7);
+		Score score5 = objective.getScore(ChatColor.GRAY + "" + Float.toString(mm.getMoney(p)));
+		score5.setScore(6);
+		Score score11 = objective.getScore("  ");
+		score11.setScore(5);
+		Score score8 = objective.getScore(ChatColor.GRAY + "" + ChatColor.BOLD + "Random Word");
+		score8.setScore(4);
 		String[] str = {"Awesome!", "Epic!", "Amazing!", "Cool!", "I caz spll", "Dude!", "EstiNet!", "Hi!", "Different!", "Oink." , "Beep!", "Welcome!", "Yo.", "LOL!", "wut", "Play!", "toes"}; 
-		Score score9 = objective.getScore(ChatColor.LIGHT_PURPLE + str[(int) Math.floor(Math.random() * 17)]);
-		score9.setScore(2);
+		Score score9 = objective.getScore(ChatColor.GRAY + str[(int) Math.floor(Math.random() * 17)]);
+		score9.setScore(3);
+		Score score100 = objective.getScore("   ");
+		score100.setScore(2);
 		if(!(p.getName().length() >= 16)){
 		Score score10 = objective.getScore(ChatColor.DARK_AQUA + p.getName().trim());
 		score10.setScore(1);
