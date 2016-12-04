41c698ede5ce40973c4d5d423cf61cc8ba654816
espidev <aguy867@gmail.com>
2016-12-03 21:33:25 -0500

    Changes

--- src/net/estinet/gFeatures/Feature/FusionPlay/Configure.java
+++ src/net/estinet/gFeatures/Feature/FusionPlay/Configure.java
@@ -2,6 +2,7 @@ package net.estinet.gFeatures.Feature.FusionPlay;
 
 import net.estinet.gFeatures.Basic;
 import net.estinet.gFeatures.Configs;
+import net.estinet.gFeatures.ClioteSky.ClioteSky;
 import net.estinet.gFeatures.Command.EstiCommand;
 import net.estinet.gFeatures.Feature.Base.Commands.TestCommand;
 public class Configure{
@@ -10,6 +11,7 @@ public class Configure{
 		FusionPlay base = new FusionPlay("FusionPlay", "1.0.0");
 		Basic.addFeature(base);
 		
-		
+		FusionPlayClioteHook fpch = new FusionPlayClioteHook(base);
+		ClioteSky.addClioteHook(fpch);
 	}
 }
--- src/net/estinet/gFeatures/Feature/FusionPlay/FusionPlayClioteHook.java
+++ src/net/estinet/gFeatures/Feature/FusionPlay/FusionPlayClioteHook.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import net.estinet.gFeatures.gFeature;
 import net.estinet.gFeatures.ClioteSky.API.ClioteHook;
-import net.estinet.gFeatures.Feature.Friendship.Friendship;
-import net.estinet.gFeatures.Feature.Friendship.Menus.FriendsPending;
 
 public class FusionPlayClioteHook extends ClioteHook{
 
@@ -15,16 +13,8 @@ public class FusionPlayClioteHook extends ClioteHook{
 	@Override
 	public void run(List<String> args, String categoryName, String clioteName){
 		try{
-			if(args.get(0).equals("done")){
-				if(Friendship.cacheNames.contains(args.get(1))){
-					Friendship.cacheNames.remove(args.get(1));
-					FriendsPending fl = new FriendsPending();
-					fl.init(Friendship.friendreq.get(args.get(1)), args.get(1));
-					Friendship.friendreq.remove(args.get(1));
-				}
-			}
-			else{
-				Friendship.friendreq.get(args.get(1)).add(args.get(0));
+			switch(args.get(0)){
+			
 			}
 		}
 		catch(Exception e){
