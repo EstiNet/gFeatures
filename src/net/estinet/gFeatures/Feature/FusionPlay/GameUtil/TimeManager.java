abaef0308644506ec0e6068e074caa41ac28f9bf
espidev <aguy867@gmail.com>
2016-12-08 20:01:49 -0500

    Changes

--- src/net/estinet/gFeatures/Feature/FusionPlay/GameUtil/TimeManager.java
+++ src/net/estinet/gFeatures/Feature/FusionPlay/GameUtil/TimeManager.java
@@ -5,7 +5,20 @@ import java.util.concurrent.TimeUnit;
 public class TimeManager {
 	private double length = 1;
 	private TimeUnit timeunit = TimeUnit.MINUTES;
-	public TimeManager(){
-		
+	public TimeManager(double length, TimeUnit timeunit){
+		this.length = length;
+		this.timeunit = timeunit;
+	}
+	public double getLength(){
+		return length;
+	}
+	public TimeUnit getTimeUnit(){
+		return timeunit;
+	}
+	public void setLength(double length){
+		this.length = length;
+	}
+	public void setTimeUnit(TimeUnit timeunit){
+		this.timeunit = timeunit;
 	}
 }
