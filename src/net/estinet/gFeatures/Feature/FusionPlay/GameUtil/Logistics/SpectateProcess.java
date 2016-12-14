7010949e9d8c053ed548e630a5156f48a885b388
espidev <aguy867@gmail.com>
2016-12-14 11:50:15 -0500

    Changes

--- src/net/estinet/gFeatures/Feature/FusionPlay/GameUtil/Logistics/SpectateProcess.java
+++ src/net/estinet/gFeatures/Feature/FusionPlay/GameUtil/Logistics/SpectateProcess.java
@@ -1,5 +1,19 @@
 package net.estinet.gFeatures.Feature.FusionPlay.GameUtil.Logistics;
 
-public class SpectateProcess {
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 
+import java.util.List;
+
+public class SpectateProcess {
+    public static List<Player> spectators = new java.util.ArrayList<>();
+
+    public static void addSpectator(Player p){
+        p.setGameMode(GameMode.ADVENTURE);
+        for(Player pp : Bukkit.getOnlinePlayers()){
+            if(!spectator)
+            pp.hidePlayer(p);
+        }
+    }
 }
