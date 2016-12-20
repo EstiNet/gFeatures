9a6aa48739b470ae54b2fbe8874d3d824737d76a
espidev <aguy867@gmail.com>
2016-12-19 19:54:06 -0500

    Changes

--- src/net/estinet/gFeatures/Feature/FusionPlay/ClioteResponse/StartResponse.java
+++ src/net/estinet/gFeatures/Feature/FusionPlay/ClioteResponse/StartResponse.java
@@ -10,7 +10,12 @@ public class StartResponse {
 	public static void response(List<String> args, String categoryName, String clioteName){
 		CliotePing cp = new CliotePing();
 		if(!FusionPlay.otherup){
-			cp.sendMessage("fusionplay cachealive", "Bungee");
+			if(args.contains("new")){
+				cp.sendMessage("fusionplay cachealive new", "Bungee");
+			}
+			else{
+				cp.sendMessage("fusionplay cachealive", "Bungee");
+			}
 		}
 		FusionPlay.assigned = true;
 		FusionPlay.currentGame.gameAssigned();
