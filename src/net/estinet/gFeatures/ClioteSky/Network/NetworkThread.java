e06c9289cbe9000d065593c2476ba8b759f7d0bc
espidev <aguy867@gmail.com>
2017-01-01 10:23:36 -0500

    ClioteSky 2017

--- src/net/estinet/gFeatures/ClioteSky/Network/NetworkThread.java
+++ src/net/estinet/gFeatures/ClioteSky/Network/NetworkThread.java
@@ -14,6 +14,25 @@ import net.estinet.gFeatures.ClioteSky.ClioteConfigUtil;
 import net.estinet.gFeatures.ClioteSky.ClioteSky;
 import net.estinet.gFeatures.ClioteSky.Network.Protocol.Output.OutputHello;
 
+/*
+gFeatures
+https://github.com/EstiNet/gFeatures
+
+   Copyright 2017 EstiNet
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 public class NetworkThread {
 	public static Socket clientSocket = null;
 	public static DataOutputStream outToServer = null;
