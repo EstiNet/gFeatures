cb4191ccf8db6973cda2cde794a52c2e3931ae98
espidev <aguy867@gmail.com>
2017-12-13 21:05:00 -0500

    wo

--- src/net/estinet/gFeatures/Feature/EstiCoins/EstiCoins.java
+++ src/net/estinet/gFeatures/Feature/EstiCoins/EstiCoins.java
@@ -12,7 +12,8 @@ import java.io.IOException;
 
 public class EstiCoins extends gFeature implements Events {
 
-    String address, port, tablename, username, password;
+    //SQL Fields
+    public static String sqlAddress, sqlPort, sqlTablename, sqlUsername, sqlPassword;
 
     public EstiCoins(String featurename, String version) {
         super(featurename, version);
@@ -24,10 +25,10 @@ public class EstiCoins extends gFeature implements Events {
             Bukkit.getLogger().info("[EstiCoins] Enabled! Yay.");
             if(cm.check()){
                 Connection c = new Connection();
-                String URL = c.toURL(port, address, tablename);
-                c.checkOnline(URL, Username, Password);
-                c.Connect(URL, Username, Password, "CREATE TABLE IF NOT EXISTS Peoples(Name VARCHAR(200), Money FLOAT) ENGINE=InnoDB;");
-                c.Connect(URL, Username, Password, "UPDATE Peoples SET Money = Money + 1\nWHERE Name = 'InDev';");
+                String URL = c.toURL(sqlPort, sqlAddress, sqlTablename);
+                c.checkOnline(URL, sqlUsername, sqlPassword);
+                c.Connect(URL, sqlUsername, sqlPassword, "CREATE TABLE IF NOT EXISTS Peoples(Name VARCHAR(200), Money FLOAT) ENGINE=InnoDB;");
+                c.Connect(URL, sqlUsername, sqlPassword, "UPDATE Peoples SET Money = Money + 1\nWHERE Name = 'InDev';");
             }
             Config con = new Config();
             con.createDirectory("plugins/gFeatures/EstiCoins", "Created EstiCoins directory!");
@@ -37,12 +38,19 @@ public class EstiCoins extends gFeature implements Events {
     }
     @Override
     public void disable(){
-        Disable.onDisable();
+        Bukkit.getLogger().info("[EstiCoins] Disabled. Good bye!");
     }
     @Override
     public void eventTrigger(Event event) {
         if(event.getEventName().equalsIgnoreCase("playerjoinevent")){
-            eh.onPlayerJoin((PlayerJoinEvent)event);
+            Connection c = new Connection();
+            Thread thr = new Thread(() -> c.Connect(c.toURL(sqlPort, sqlAddress, sqlTablename), cc.getUsername(), cc.getPassword(), "INSERT INTO Peoples(Name, Money)\n"+
+                    "SELECT * FROM (SELECT '" + e.getPlayer().getUniqueId() + "', '0') AS tmp\n"+
+                    "WHERE NOT EXISTS (\n"+
+                    "SELECT Name FROM Peoples WHERE Name = '" + e.getPlayer().getUniqueId() + "'\n"+
+                    ") LIMIT 1;\n"
+            ));
+            thr.start();
         }
     }
     @Retrieval
