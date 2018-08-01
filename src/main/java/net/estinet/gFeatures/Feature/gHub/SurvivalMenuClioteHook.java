701caab25e9888579ba850bdd16cebdbee2ad892
EspiDev <espidev@gmail.com>
2018-07-31 21:07:27 -0400

    Merged with 1.13 branch gHub

--- src/main/java/net/estinet/gFeatures/Feature/gHub/SurvivalMenuClioteHook.java
+++ src/main/java/net/estinet/gFeatures/Feature/gHub/SurvivalMenuClioteHook.java
@@ -14,11 +14,22 @@ public class SurvivalMenuClioteHook extends ClioteHook {
         this.gFeatureName = gFeatureName;
     }
 
+    public static int i = 0, taskID;
+
     @Override
     public void run(byte[] data, String sender) {
         List<String> args = ClioteSky.parseBytesToStringList(data);
-        Bukkit.getScheduler().runTaskLater(Bukkit.getPluginManager().getPlugin("gFeatures"), () -> {
-            Bukkit.getPlayer(args.get(0)).teleport(new Location(Bukkit.getPlayer(args.get(0)).getWorld(), -158.5, 105.5, 485.5));
-        }, 30);
+        Runnable run = () -> {
+            if (i >= 20) {
+                Bukkit.getScheduler().cancelTask(taskID);
+            }
+            if (Bukkit.getPlayer(args.get(0)) != null) {
+                Bukkit.getPlayer(args.get(0)).teleport(new Location(Bukkit.getPlayer(args.get(0)).getWorld(), -158.5, 105.5, 485.5));
+                Bukkit.getScheduler().cancelTask(taskID);
+            } else {
+                i++;
+            }
+        };
+        SurvivalMenuClioteHook.taskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(Bukkit.getPluginManager().getPlugin("gFeatures"), run, 20, 20);
     }
 }
