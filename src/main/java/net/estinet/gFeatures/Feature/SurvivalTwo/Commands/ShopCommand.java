176411e4b1f39a454408a14471138ce31e43c906
EspiDev <espidev@gmail.com>
2019-04-30 13:58:24 -0400

    Updated license

--- src/main/java/net/estinet/gFeatures/Feature/SurvivalTwo/Commands/ShopCommand.java
+++ src/main/java/net/estinet/gFeatures/Feature/SurvivalTwo/Commands/ShopCommand.java
@@ -6,23 +6,24 @@ import net.estinet.gFeatures.Command.CommandExecutable;
 import net.estinet.gFeatures.Feature.SurvivalTwo.Menus.Shop;
 
 /*
-gFeatures
-https://github.com/EstiNet/gFeatures
-
-   Copyright 2018 EstiNet
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+ * gFeatures
+ * https://github.com/EstiNet/gFeatures
+ *
+ *    Copyright 2019 EstiNet
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
 
 public class ShopCommand extends CommandExecutable{
 	@Override
--- src/main/java/net/estinet/gFeatures/Feature/gScore/Scored.java
+++ src/main/java/net/estinet/gFeatures/Feature/gScore/Scored.java
@@ -1,24 +1,5 @@
 package net.estinet.gFeatures.Feature.gScore;
 
-/*
-gFeatures
-https://github.com/EstiNet/gFeatures
-
-   Copyright 2018 EstiNet
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 import net.estinet.gFeatures.API.EssentialsHook.EssentialsEcoUtil;
 import net.estinet.gFeatures.Feature.EstiCoins.EstiCoins;
 import org.bukkit.Bukkit;
@@ -33,6 +14,26 @@ import org.bukkit.scoreboard.ScoreboardManager;
 import net.estinet.gFeatures.Feature.ServerQuery.ServerQuery;
 
 import java.io.File;
+
+/*
+ * gFeatures
+ * https://github.com/EstiNet/gFeatures
+ *
+ *    Copyright 2019 EstiNet
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
 
 public class Scored {
 	public static Scoreboard getScore(Player p, Scoreboard board) throws IllegalStateException, IllegalArgumentException{
